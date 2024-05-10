cc24ee96388c0b1d53bc8e6a78996752fa0108f9
zrooty <zaky.zni@gmail.com>
2024-05-10 08:57:07 +0700

    [ihsnz] test passed

--- routes/api.php
+++ routes/api.php
@@ -24,8 +24,8 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     // Keep in mind that prefix should be /api/v1.
     // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
-    Route::group(['prefix' => '/v1'], function () {
-        Route::apiResource('task', TaskController::class);
+    Route::prefix('/v1')->name('v1.')->group(function() {
+        Route::apiResource('/tasks', TaskController::class);
     });
 
 });
